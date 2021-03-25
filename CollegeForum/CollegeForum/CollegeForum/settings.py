22be4e5078ddfd050f7d306731000e9885bbebbd
Varba*56772 <varunbarve94@gmail.com>
2021-03-25 21:21:16 +0530

    Custum user model part 1

--- CollegeForum/CollegeForum/CollegeForum/settings.py
+++ CollegeForum/CollegeForum/CollegeForum/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'authentication',
 ]
 
 MIDDLEWARE = [
@@ -101,6 +102,8 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
+
+AUTH_USER_MODEL = 'authentication.CustomUser'
 
 
 # Internationalization
