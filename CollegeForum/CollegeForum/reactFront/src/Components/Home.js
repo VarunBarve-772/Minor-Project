df3c5c54837050a7074a6e72210e4c5f4d9b2057
Vaibhav A. Porwal <vaibhav83porwal@gmail.com>
2021-04-01 20:46:24 +0530

    added styling in main page.

--- CollegeForum/CollegeForum/reactFront/src/Components/Home.js
+++ CollegeForum/CollegeForum/reactFront/src/Components/Home.js
@@ -1,75 +1,81 @@
 import React from 'react';
 function Home() {
     return(
-        <div className="">
-        <div class="sec-1" >
-  <div class="row">
-    <div class="col-lg-6">
-      <h1 class="title">College Forum</h1>
-      {/* <img class="logo-img" src="Images/logo.png"/> */}
-  
-    </div>
-<div class="col-lg-6">
-  <div class="topnav ">
-    <div class="topnav-right" >
-      <a href="#Questions"> Ask Questions</a>
-      <a href="#">Answer Questions</a> 
-      <a href="#about-us">About-us</a> 
-      <a href="#profile">Student Profile</a>
+<div class="">
 
+  <div class="sec-1" >
+    <div class="row">
+      <div class="col-lg-4">
+        <h1 class="title">College Forum</h1>
+        {/* <img class="logo-img" src="Images/logo.png"/> */}  
+      </div>
+
+      <div class="col-lg-8">
+        <div class="topnav">
+          <div class="topnav-right" >
+            <a href="#Questions"> Ask Questions</a>
+            <a href="#">Answer Questions</a> 
+            <a href="#about-us">About-us</a> 
+            <a href="#profile">Student Profile</a>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
-</div>
 
+  <div class="sec-2">
+    <div class="row">
+      <div class="col-sm-2 sec-2-col text-center">
+        <a href="#general" active >General</a>
+      </div>
+
+      <div class="col-sm-2 sec-2-col text-center">
+        <a href="#computer science">Computer Science</a>
+      </div>
+
+      <div class="col-sm-2 sec-2-col text-center">
+        <a href="#information technology">Information Technology</a>
+      </div>
+
+      <div class="col-sm-2 sec-2-col text-center">
+        <a href="#mechanical">Mechanical</a>
+      </div>
+
+      <div class="col-sm-2 sec-2-col text-center">
+        <a href="#civil">Civil</a>
+      </div>
+
+      <div class="col-sm-2 sec-2-col text-center">
+        <a href="#electronics">Electronics</a>
+      </div>
+    </div>
+  </div>
+
+  <div class="sec-3">
+    <div class="sec-3-div">
+      <h6>How i can fill academic fees through erp?</h6>
+      <p>Go to google and search for svv erp then login to it after that go to academics you will find an option for fees deposit.</p>
+    </div>
+
+    <div class="sec-3-div">
+      <h6>How i can fill academic fees through erp?</h6>
+      <p>Go to google and search for svv erp then login to it after that go to academics you will find an option for fees deposit.</p>
+    </div>
+
+    <div class="sec-3-div">
+      <h6>How i can fill academic fees through erp?</h6>
+      <p>Go to google and search for svv erp then login to it after that go to academics you will find an option for fees deposit.</p>
+    </div>
+
+    <div class="sec-3-div">
+      <h6>How i can fill academic fees through erp?</h6>
+      <p>Go to google and search for svv erp then login to it after that go to academics you will find an option for fees deposit.</p>
+    </div>
   </div>
 
 </div>
-<div class="sec-2">
-<div class="row">
-    <div class="col-sm-2 sec-2-col">
-    <a href="#profile" active >General</a>
-    </div>
-<div class="col-sm-2 sec-2-col">
-<a href="#profile">Computer Science</a>
-    </div>
-    <div class="col-sm-2 sec-2-col">
-    <a href="#profile">Information Technology</a>
-    </div>
-    <div class="col-sm-2 sec-2-col">
-    <a href="#profile">Mechanical</a>
-    </div>
-    <div class="col-sm-2 sec-2-col">
-    <a href="#profile">Civil</a>
-    </div>
-    <div class="col-sm-2 sec-2-col">
-    <a href="#profile">Electronics</a>
-    </div>
-  </div>
-</div>
-<div class="sec-3">
-<div class="sec-3-div">
-<h5>Q:-How i can fill academic fees through erp?</h5>
-<h5>A:-Go to google and search for svv erp then login to it after that go to academics you will find an option for fees ddeposit.</h5>
-</div>
-<div class="sec-3-div">
-<h5>Q:-How i can fill academic fees through erp?</h5>
-<h5>A:-Go to google and search for svv erp then login to it after that go to academics you will find an option for fees ddeposit.</h5>
-</div>
-<div class="sec-3-div">
-<h5>Q:-How i can fill academic fees through erp?</h5>
-<h5>A:-Go to google and search for svv erp then login to it after that go to academics you will find an option for fees ddeposit.</h5>
-</div>
-<div class="sec-3-div">
-<h5>Q:-How i can fill academic fees through erp?</h5>
-<h5>A:-Go to google and search for svv erp then login to it after that go to academics you will find an option for fees ddeposit.</h5>
-</div>
-</div>
-
-  </div>
-
-
 
     );
-
 }
+
 export default Home;
